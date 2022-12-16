09e2c8d4d964c7ab5077020044ae340d429db5f3
P-Ade <109440726+P-Ade@users.noreply.github.com>
2022-12-15 16:32:27 -0800

    Update errors.c

--- errors.c
+++ errors.c
@@ -29,7 +29,7 @@ void _eputs(char *str)
 int _eputchar(char c)
 {
 	static int i;
-	sttic char buf[WRITE_BUF_SIZE];
+	static char buf[WRITE_BUF_SIZE];
 
 	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
