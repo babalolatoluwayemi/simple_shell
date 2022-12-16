ed28304c49733b2aa2b35e69c68e3ddbb10a3592
P-Ade <109440726+P-Ade@users.noreply.github.com>
2022-12-15 16:31:23 -0800

    Update environ.c

--- environ.c
+++ environ.c
@@ -29,7 +29,7 @@ char *_getenv(info_t *info, const char *name)
 		p = starts_with(node->str, name);
 		if (p && *p)
 			return (p);
-		node = node->next;
+		node = nde->next;
 	}
 	return (NULL);
 }
