77ee129c9be8490ea54501bf9e565401c94765b4
P-Ade <109440726+P-Ade@users.noreply.github.com>
2022-12-15 16:51:41 -0800

    Update shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -154,7 +154,7 @@ void fork_cmd(info_t *info)
 		{
 			info->status = WEXITSTATUS(info->status);
 			if (info->status == 126)
-				prnt_error(info, "Permission denied\n");
+				print_error(info, "Permission denied\n");
 		}
 	}
 }
